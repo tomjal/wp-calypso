34763c11d01d11e057b22b3242ec89a306508413
Leif Singer <git@singer.sh>
2016-04-29 14:16:33 +0200

    use the `preview` layoutFocus for the current site card in the sidebar

--- client/my-sites/sidebar/sidebar.jsx
+++ client/my-sites/sidebar/sidebar.jsx
@@ -53,6 +53,16 @@ module.exports = React.createClass( {
 		window.scrollTo( 0, 0 );
 	},
 
+	onCustomize: function( event ) {
+		event.preventDefault();
+		this.props.layoutFocus.set( 'preview' );
+	},
+
+	onPreviewSite( event ) {
+		event.preventDefault();
+		this.props.layoutFocus.set( 'preview' );
+	},
+
 	itemLinkClass: function( paths, existingClasses ) {
 		var classSet = {};
 
@@ -715,6 +725,7 @@ module.exports = React.createClass( {
 				<CurrentSite
 					sites={ this.props.sites }
 					siteCount={ this.props.user.get().visible_site_count }
+					onClick={ this.onPreviewSite }
 				/>
 				<SidebarMenu>
 					<ul>

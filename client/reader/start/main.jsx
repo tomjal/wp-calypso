ac4b4ad55c784d299fc26d47edea3f21ec399943
bluefuton <chris@bluefuton.com>
2016-05-06 15:17:57 +0100

    Reader Start: add components and basic styles

--- client/reader/start/main.jsx
+++ client/reader/start/main.jsx
@@ -1,9 +1,14 @@
 // External dependencies
 import React from 'react';
+import debugModule from 'debug';
 import { connect } from 'react-redux';
 
 // Internal dependencies
 import Main from 'components/main';
+import Button from 'components/button';
+import StartCard from './card';
+
+const debug = debugModule( 'calypso:reader:start' ); //eslint-disable-line no-unused-vars
 
 const Start = React.createClass( {
 	render() {
@@ -11,7 +16,17 @@ const Start = React.createClass( {
 			<Main className="reader-start">
 				<header>
 					<h2>{ this.translate( 'Welcome to the Reader' ) }</h2>
+					<Button disabled primary>{ this.translate( "OK, I'm all set!" ) }</Button>
 				</header>
+				<p>{ this.translate( "Discover great stories and read your favourite sites' posts all in one place." ) }&nbsp;
+				{ this.translate( "Every time there are new updates to the sites you follow, you'll be the first to know!" ) }
+				</p>
+
+				<div className="reader-start__cards">
+					<StartCard />
+					<StartCard />
+					<StartCard />
+				</div>
 			</Main>
 		);
 	}

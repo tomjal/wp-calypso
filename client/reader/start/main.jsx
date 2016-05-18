fa6fc474f4ea0ef0116ce0309bcca040ab874ec9
jancavan <janmcavan@gmail.com>
2016-05-18 15:22:17 -0700

    Add done bar to bottom

--- client/reader/start/main.jsx
+++ client/reader/start/main.jsx
@@ -16,7 +16,6 @@ const Start = React.createClass( {
 			<Main className="reader-start">
 				<header className="reader-start__intro">
 					<h1>{ this.translate( 'Welcome to the Reader' ) }</h1>
-					<Button disabled primary>{ this.translate( "OK, I'm all set!" ) }</Button>
 					<p>{ this.translate( "Discover great stories and read your favorite sites' posts all in one place. Every time there are new updates to the sites you follow, you'll be the first to know!" ) }
 				</p>
 				</header>
@@ -25,6 +24,15 @@ const Start = React.createClass( {
 					<StartCard />
 					<StartCard />
 					<StartCard />
+					<StartCard />
+					<StartCard />
+				</div>
+
+				<div className="reader-start-footer">
+					<div className="reader-start-footer__action main">
+						<span>Follow one or more sites to get started</span>
+						<Button disabled>{ this.translate( "OK, I'm all set!" ) }</Button>
+					</div>
 				</div>
 			</Main>
 		);
